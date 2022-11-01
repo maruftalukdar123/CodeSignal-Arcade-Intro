c990524e25acbe30e0ff5ab5d26ce1699dc0c921
Maruf Talukdar <maruf.talukdar.19@ucl.ac.uk>
2022-11-01 10:49:44 +0000

    Added comments explaining the solutions to the following problem:
    1. Common Character Count
    2. All Longest Strings.

--- All Longest Strings/Solution.py
+++ All Longest Strings/Solution.py
@@ -1,6 +1,11 @@
 from more_itertools import locate
 
 def solution(inputArray):
+
+    # Find the lengths of each element in array
+    # Find the indeces where the element with the maximum number characters is 
+    # Output inputarray[indeces]
+
     lengths = [len(el) for el in inputArray]
     indeces = list(locate(lengths,lambda x: x==max(lengths)))
     solution = [inputArray[i] for i in indeces]
--- Common Character Count/Solution.py
+++ Common Character Count/Solution.py
@@ -1,4 +1,10 @@
 def solution(s1, s2):
+
+    # Split both inputs into its characters
+    # If character a in s2 is in s1, then append to common array
+    # Then remove that character from s1 to avoid double counting
+    # Loop for all characters in s2
+
     s1,s2 = [*s1],[*s2]
     common = []
     
@@ -7,7 +13,7 @@ def solution(s1, s2):
             common.append(ch)
             s1.remove(ch)
 
-    return len(common)
+    return print(common)
 
 s1 = "aabcc"
 s2 = "adcaa"
